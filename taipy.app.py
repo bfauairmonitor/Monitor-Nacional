ef60fb7544175286bbdea678a111f22654d3d48f
bfauairmonitor <bfa.uair.monitor@gmail.com>
2026-04-30 18:09:31 -0400

    Update taipy.app.py

--- taipy.app.py
+++ taipy.app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-import base64
 import time
 from pathlib import Path
 from datetime import datetime, timedelta, timezone
