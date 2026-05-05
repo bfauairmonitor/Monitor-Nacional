d500e30f0c065a322646532f51ca0caaf95994d4
bfauairmonitor <bfa.uair.monitor@gmail.com>
2026-05-05 15:29:35 -0400

    Update dashboardbcv.py

--- dashboardbcv.py
+++ dashboardbcv.py
@@ -59,7 +59,7 @@ html, body, .main {{
     min-height: 40px; 
 }}
 .stMainBlockContainer {{
-    padding:96px 10px 0px 10px
+    padding:96px 15px 0px 15px
     }}
 </style>
 """, unsafe_allow_html=True)
